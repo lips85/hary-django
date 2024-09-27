1a94817cc89ab7397135b83481093b77c7b3d5bd
haru <godjaewook@naver.com>
2024-09-27 10:12:13 +0900

    first commit

---
+++ tweets/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Tweet, Like
+
+
+# Register your models here.
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ("payload", "users", "created_at", "updated_at")
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ("tweet", "user", "created_at", "updated_at")
---
+++ tweets/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# commonmodel
+from common.models import CommonModel
+
+
+# Create your models here.
+class Tweet(CommonModel):
+    # payload: Text(max. lenght 180
+    payload = models.CharField(max_length=180)
+    users = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.payload
+
+
+class Like(CommonModel):
+    tweet = models.ForeignKey("tweets.Tweet", on_delete=models.CASCADE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} likes {self.tweet}"
